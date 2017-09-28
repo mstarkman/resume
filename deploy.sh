15150098a8738901bff4a9284599e05d1c988986
Mark Starkman <mrstarkman@gmail.com>
2017-09-28 07:50:07 -0400

    chore: setup deploying

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ssh markstarkman.com rm -rf apps/resume-react.markstarkman.com/*
+
+scp out/index.html markstarkman.com:apps/resume-react.markstarkman.com
+scp -r out/_next markstarkman.com:apps/resume-react.markstarkman.com
