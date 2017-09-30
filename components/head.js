e31b98d13dfa281ed4111b75797cbce432ead6e2
Mark Starkman <mrstarkman@gmail.com>
2017-09-30 10:27:37 -0400

    feat: add favicon

--- components/head.js
+++ components/head.js
@@ -7,6 +7,20 @@ const Head = () => (
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <meta name="description" content="Mark Starkman's Resume" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+
+    {/* Add to homescreen for Chrome on Android */}
+    <meta name="mobile-web-app-capable" content="yes" />
+    <link rel="icon" sizes="192x192" href="/static/img/android-desktop.png" />
+
+    {/* Add to homescreen for Safari on iOS  */}
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="apple-mobile-web-app-title" content="Mark Starkman's Resume" />
+    <link rel="apple-touch-icon-precomposed" href="/static/img/ios-desktop.png" />
+
+    {/* Add favicon */}
+    <link rel="shortcut icon" href="/static/img/favicon.png" />
+
     <title>Mark Starkman&#39;s Resume</title>
     <link
       rel="stylesheet"
