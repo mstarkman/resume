4b2e913c8e7310ffc854adcf870faf22f06f2ee6
Mark Starkman <mrstarkman@gmail.com>
2017-10-01 14:59:54 -0400

    feat: add resume sections

---
+++ components/education.js
@@ -0,0 +1,7 @@
+const Education = () => (
+  <div className="content">
+    <h3>Education</h3>
+  </div>
+)
+
+export default Education
---
+++ components/experience.js
@@ -0,0 +1,7 @@
+const Experience = () => (
+  <div className="content">
+    <h3>Experience</h3>
+  </div>
+)
+
+export default Experience
