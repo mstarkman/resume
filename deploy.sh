5992136c0acd37186af7ba692095c2b9e31bf9b3
Mark Starkman <mrstarkman@gmail.com>
2017-10-01 10:35:39 -0400

    chore: deploy static directory

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ ssh markstarkman.com rm -rf apps/resume-react.markstarkman.com/*
 
 scp out/index.html markstarkman.com:apps/resume-react.markstarkman.com
 scp -r out/_next markstarkman.com:apps/resume-react.markstarkman.com
+scp -r out/static markstarkman.com:apps/resume-react.markstarkman.com
