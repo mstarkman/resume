b182edfb634f6ac72eb0f71772fd29f7a6618e1a
Mark Starkman <mrstarkman@gmail.com>
2017-10-01 18:13:31 -0400

    feat: create the education section

---
+++ components/details.js
@@ -0,0 +1,66 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Row = styled.div`
+  display: flex;
+`
+
+const Left = styled.div`
+  align-self: flex-start;
+`
+
+const Right = styled.div`
+  align-self: flex-end;
+  margin-left: auto;
+`
+
+const Title = styled.div`
+  font-size: 1.25rem;
+`
+
+const Place = styled.div`
+  font-style: italic;
+`
+
+const Details = ({ bullets, dates, location, place, title }) => (
+  <div>
+    <Row>
+      <Left>
+        <Title>
+          {title}
+        </Title>
+      </Left>
+      <Right>
+        <div>
+          {dates}
+        </div>
+      </Right>
+    </Row>
+    <Row>
+      <Left>
+        <Place>
+          {place}
+        </Place>
+      </Left>
+      <Right>
+        <div>
+          {location}
+        </div>
+      </Right>
+    </Row>
+  </div>
+)
+
+Details.propTypes = {
+  bullets: PropTypes.arrayOf(PropTypes.string),
+  dates: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  place: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+}
+
+Details.defaultProps = {
+  bullets: [],
+}
+
+export default Details
--- components/education.js
+++ components/education.js
@@ -1,6 +1,14 @@
+import Details from './details'
+
 const Education = () => (
   <div className="content">
     <h3>Education</h3>
+    <Details
+      dates="Graduated May 1996"
+      location="Pittsburgh, PA"
+      place="University of Pittsburgh"
+      title="Bachelor of Science – Electrical Engineering"
+    />
   </div>
 )
 
